f251f6928a3c94445d0e4bf83f44abd924509401
sowjanya006 <sowjanyakota678@gmail.com>
2020-06-04 10:15:24 +0530

    first commit

---
+++ tessy/work/00000401/00000402/00000403/00000404/ts_comm_env.h
@@ -0,0 +1,8 @@
+#ifndef __TS_COMM_ENV_H__
+#define __TS_COMM_ENV_H__
+#define TS_TESTOBJECT_FOLDER  "C:/Users/Sowjanya/Desktop/GIT_FO~1/tessy\\work\\00000401\\00000402\\00000403\\00000404"
+#define TS_PROJECT_ID         1024
+#define TS_MODULE_ID          1027
+#define TS_TESTOBJECT_ID      1028
+#endif
+
